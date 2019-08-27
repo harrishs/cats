de96ef98b3c42a40596eade527dd85dddd2b6146
Harrish S <harrish.s@hotmail.ca>
2019-08-27 19:35:14 +0000

    asynchronous vs synchronous functions with testing

--- syncBreeds.js
+++ syncBreeds.js
@@ -1,18 +1,18 @@
-// asyncBreeds.js
-const fs = require("fs");
-
-const breedDetailsFromFile = function(breed, callback) {
-  console.log("breedDetailsFromFile: Calling readFile...");
-  fs.readFile(`./data/${breed}.txt`, "utf8", (error, data) => {
-    // ISSUE: Returning from inner callback function, not our main function.
-    console.log("Callback: I have the data!");
-    if (!error) return callback(data);
-  });
-  // ISSUE: Attempting to return data out here will also not work.
-  //        Currently not returning anything from here, so this function returns undefined.
+// data in memory
+const catBreeds = {
+  Balinese:
+    "Balinese are curious, outgoing, intelligent cats with excellent communication skills. They are known for their chatty personalities and are always eager to tell you their views on life, love, and what you’ve served them for dinner.",
+  Bombay:
+    "The golden eyes and the shiny black coat of the Bombay is absolutely striking. Likely to bond most with one family member, the Bombay will follow you from room to room and will almost always have something to say about what you are doing, loving attention and to be carried around, often on his caregiver's shoulder."
 };
 
-// we try to get the return value
-breedDetailsFromFile("Bombay", bombay => {
-  console.log("Return Value: ", bombay); // => will NOT print out details, instead we will see undefined!
-});
+// synchronous function to fetch a cat breed
+const breedDetails = function(breed) {
+  // can simply return it (easy peezee, butter squeezy) ...
+  return catBreeds[breed];
+};
+
+// get the return value right away from the function
+const bombay = breedDetails("Bombay");
+console.log(bombay); //=> prints out the description for that breed
+module.exports = breedDetails;
